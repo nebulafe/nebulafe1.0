a542a0fed8d801cbd549e0d99e1463a9ed8eeeaa
chunqi.wcq <chunqi.wcq@alibaba-inc.com>
2015-07-18 18:55:01 +0800

    video page and view update

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp'),
 	imagemin = require('gulp-imagemin'),
 	del = require('del'),
-	path = require('path');
+	path = require('path'),
+	sass = require('gulp-ruby-sass'),
+	livereload = require('gulp-livereload'),
+	notify = require('gulp-notify');
 
 gulp.task('img',function() {
 	var img_src = './www/resource/img/*';
@@ -10,7 +13,16 @@ gulp.task('img',function() {
         .pipe(imagemin())
         .pipe(gulp.dest(img_des));
 });
-
+gulp.task('styles', function() {
+	return sass( './www/resource/scss/pages', { style: 'expanded' })
+		.pipe(gulp.dest('./www/resource/css'))
+		.pipe(livereload())
+		.pipe(notify({ message: 'SCSS编译完成' }));
+});
 gulp.task('default', function() {
   return gulp.start('img')
+});
+gulp.task('watch', function() {
+	livereload.listen();
+	gulp.watch('./www/resource/scss/pages/*.scss', ['styles']);
 });
