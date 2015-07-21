04f90526e5a6ae1dcd4a070f52d4b9be6acd4ce4
yuheng.wpf <yuheng.wpf@alibaba-inc.com>
2015-07-21 17:19:12 +0800

    add files by wangpengfei

--- App/Conf/route.js
+++ App/Conf/route.js
@@ -7,5 +7,6 @@ module.exports = [
   [/^logout\/?$/, "admin/index/logout"],
   ["user/see/:id", "user/see"],
   ["course/view/:id", "course/view"],
+  ["course/video/:id/:v_id", "course/video"],
   ["partner/view/:id", "partner/view"]
 ]
