9d9ee0fe80785a6fc50dd60db8ba33e8fa09589b
yuheng.wpf <yuheng.wpf@alibaba-inc.com>
2015-07-25 21:21:13 +0800

    add files by wangpengfei

--- www/resource/js/user/verifyemail.js
+++ www/resource/js/user/verifyemail.js
@@ -1,12 +1,24 @@
 define(function(require, exports, moudle) {
   $(function(){
     $('#verify_email').on('click',function(e){
+      var email = $('#user_email').html();
       $.post('user/verifyemail', {
         verifycode:$('#verify_code').val(),
-        email : $('#user_email').html()
+        email :email
       },function(data){
-        console.log(data)
+        if(data.errno == "0"){
+          $("div.my-wrap").hide();
+          $(".js-forgot-result").show().find("em").text(email);
+          ALERT('提示','发送成功，请查收邮件！');
+        }else{
+          ALERT('错误',data.errmsg);
+        }
       })
-    })
+    });
+    $("span.change-code").click(function(){
+      var $p=$(this).prev("img");
+      $p.attr("src",$p.attr("src").replace(/\?\d+|$/,"?"+(new Date()).getTime()));
+    });
+
   })
 })
