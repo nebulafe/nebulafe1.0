1037a55f21715359fa0c0a6f9e70d0804c4fe61f
yuheng.wpf <yuheng.wpf@alibaba-inc.com>
2015-07-25 15:14:17 +0800

    add files by wangpengfei

--- App/Conf/route.js
+++ App/Conf/route.js
@@ -9,6 +9,7 @@ module.exports = [
   ["user/avator/:id", "user/avator"],
   ["user/verifyemail/:id", "user/verifyemail"],
   ["user/setpwd/:id", "user/setpwd"],
+  ["user/mycourse/:id", "user/mycourse"],
   ["course/view/:id", "course/view"],
   ["course/video/:id/:v_id", "course/video"],
   ["partner/view/:id", "partner/view"]
