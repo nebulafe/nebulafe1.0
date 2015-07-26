480fceffe0001a80a052112da574a10244eb04c4
yuheng.wpf <yuheng.wpf@alibaba-inc.com>
2015-07-26 11:59:56 +0800

    add files by wangpengfei

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
 	imagemin = require('gulp-imagemin'),
 	del = require('del'),
 	path = require('path'),
-	sass = require('gulp-ruby-sass'),
+	sass = require('gulp-sass'),
 	livereload = require('gulp-livereload'),
 	notify = require('gulp-notify');
 
@@ -14,15 +14,15 @@ gulp.task('img',function() {
         .pipe(gulp.dest(img_des));
 });
 gulp.task('styles', function() {
-	return sass( './www/resource/scss/pages', { style: 'expanded' })
+	return gulp.src('./www/resource/scss/pages/**/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}))
 		.pipe(gulp.dest('./www/resource/css'))
-		.pipe(livereload())
 		.pipe(notify({ message: 'SCSS编译完成' }));
 });
 gulp.task('default', function() {
-  return gulp.start('img')
+  return gulp.start('styles')
 });
 gulp.task('watch', function() {
 	livereload.listen();
-	gulp.watch('./www/resource/scss/pages/*.scss', ['styles']);
+	gulp.watch('./www/resource/scss/pages/**/*.scss', ['styles']);
 });
