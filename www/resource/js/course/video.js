ee8a4421c9e27ed6b6c092cd63789cc28b9b51bd
yuheng.wpf <yuheng.wpf@alibaba-inc.com>
2015-07-26 11:08:05 +0800

    add files by wangpengfei

--- www/resource/js/course/video.js
+++ www/resource/js/course/video.js
@@ -2,27 +2,6 @@ define(function (require, exports, moudle) {
     alifenxi.track("course_video_view", {
         "course_name": $('#course_name').html()
     });
-    //require("js/widget/jquery.jplayer.min.js");
-    //require("js/widget/jplayer.playlist.min.js");
-    //$(function () {
-    //    new jPlayerPlaylist({
-    //        jPlayer: "#jquery_jplayer_1",
-    //        cssSelectorAncestor: "#jp_container_1"
-    //
-    //    }, res, {
-    //        swfPath: "../widget",
-    //        supplied: "webmv, ogv, m4v",
-    //        useStateClassSkin: true,
-    //        autoBlur: false,
-    //        smoothPlayBar: true,
-    //        keyEnabled: true,
-    //        size: {
-    //            width: "640px",
-    //            height: "360px",
-    //            cssClass: "jp-video-360p"
-    //        }
-    //    });
-    //});
 
     var commentBtn = $("#comment-submit");
     var commentArea = $("#user-comment-editor");
