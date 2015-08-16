769e6cdd2665000319ab03809bf2588681e3e58c
wungcq <wangchunqibuaa@163.com>
2015-08-16 10:33:53 +0800

    add static files

--- App/View/Admin/controllers.js
+++ App/View/Admin/controllers.js
@@ -1,39 +1,4 @@
 /**
- * Created by wungcq on 15/8/4.
- */
-angular.module('Manage', [])
-    .controller('login',
-    ['$scope', '$http', function ($scope, $http) {
-        $scope.userInfo = {
-            email: "",
-            pwd: ""
-        };
-        var checkForm = function(){
-            if($scope.userInfo.email.length<4 || $scope.userInfo.pwd.length < 4){
-                return false;
-            }
-            return true;
-        };
-
-        $scope.login = function () {
-            if(checkForm()){
-                var req = $http({
-                    method: 'POST',
-                    url: '/login',
-                    data: $scope.userInfo
-
-                });
-                req.success(function () {
-                    setTimeout(function(){
-                        location.href = "/manage";
-                    }, 500)
-                });
-            }
-        }
-
-    }]
-);
-/**
  * Created by wungcq on 15/8/3.
  */
 
