fbd016d4c6af54d54b7b6749cbe57fd1aaf5d300
yuheng.wpf <yuheng.wpf@alibaba-inc.com>
2015-08-16 11:24:35 +0800

    add files by wangpengfei

---
+++ App/Lib/Controller/Admin/CourseController.js
@@ -0,0 +1,21 @@
+/**
+ * controller
+ * @return
+ */
+module.exports = Controller("Admin/BaseController", function() {
+  "use strict";
+
+
+  var Service = require("../../Service/Service");
+  var oss = require("../../Service/Oss");
+
+
+  return {
+    indexAction: function(){
+      var self = this;
+      if(self.userInfo && self.userInfo.isAdmin == 1){
+        self.display()
+      }
+    }
+  };
+});
