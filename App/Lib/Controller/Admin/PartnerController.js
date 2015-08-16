889a9a6ada852bf671f9d7cfcd038289a58aea1e
yuheng.wpf <yuheng.wpf@alibaba-inc.com>
2015-08-16 12:41:53 +0800

    add files by wangpengfei

--- App/Lib/Controller/Admin/PartnerController.js
+++ App/Lib/Controller/Admin/PartnerController.js
@@ -14,10 +14,29 @@ module.exports = Controller("Admin/BaseController", function() {
     indexAction: function(){
       var self = this;
       if(self.userInfo && self.userInfo.isAdmin == 1){
-        self.assign({
-          title : "管理后台-机构管理"
-        })
-        self.display()
+        if(self.isGet()){
+          self.assign({
+            title : "管理后台-机构管理"
+          })
+          self.display()
+        }else if(self.isPost()){
+          var data = self.post();
+          var banner = self.file('banner');
+          var logo = self.file('logo');
+          getPromise([oss.put(banner ,{bucket:'n-partner', key : 'banner/'}),oss.put(logo , {bucket:'n-partner', key : 'logo/'})]).then(function(b_data,l_data){
+            Service.addPatner({
+              'banner' : banner.originalFilename,
+              'logo' : logo.originalFilename,
+              'name ' : data.name,
+              'introduction' : data.introduction
+            }).then(function(content){
+              self.success(content)
+            })
+          }).catch(function(err){
+            self.error(err)
+          })
+
+        }
       }
     }
   };
