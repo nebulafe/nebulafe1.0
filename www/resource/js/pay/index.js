204475466dcbf1a40fd54e40df63ed5c3c0657ef
wungcq <wangchunqibuaa@163.com>
2015-10-09 21:35:01 +0800

    add pay index style

--- www/resource/js/pay/index.js
+++ www/resource/js/pay/index.js
@@ -1,5 +1,23 @@
 define(function(require, exports, moudle) {
     $('#pay_btn').click(function(e){
         $.post('pay/info')
-    })
+    });
+
+    var payMethodContainer = $('#pay_method_wrapper');
+    $('#pay_method_wrapper input').get(0).checked = true;
+    payMethodContainer.on('change',function(ev){
+       var method = parseInt( $('#pay_method_wrapper input:checked').val());
+        if(method == 2){
+            $('#banks').show();
+        }else{
+            $('#banks').hide();
+        }
+        alert(method);
+    });
+
+    $('#banks input').get(0).checked = true;
+    $('#banks').on('change',function(e){
+        var bank =  $('#banks input:checked').attr("id");
+        alert(bank);
+    });
 })
