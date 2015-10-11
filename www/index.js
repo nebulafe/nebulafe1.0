97ca6aff9998d17df635bae39b12827e4e56e95f
yuheng.wpf <yuheng.wpf@alibaba-inc.com>
2015-10-11 11:25:49 +0800

    add files by wangpengfei

--- www/index.js
+++ www/index.js
@@ -5,6 +5,8 @@ global.APP_PATH = path.dirname(__dirname) + '/App';
 global.RESOURCE_PATH = __dirname;
 //网站根目录
 global.ROOT_PATH = __dirname;
+//LOG目录
+global.LOG_PATH = __dirname + '/App/Runtime/Log'
 //开启调试模式，线上环境需要关闭调试功能
 global.APP_DEBUG = false;
 //加载thinkjs启动服务
